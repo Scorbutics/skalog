fd18f7e65736bd5b71d9eff3ec5c8a3ce1a4b303
Scorbutics <brice.bulgarelli@outlook.com>
2018-10-28 18:13:50 +0100

    Premier use-case : multi-thread (n'est pas un réel TU)

--- test/src/MultiLogger_multiThreading_test.cpp
+++ test/src/MultiLogger_multiThreading_test.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <thread>
+#include <mutex>
 
 #include <doctest.h>
 #include <Logging/MultiLogger.h>
 #include <Logging/LogAsync.h>
 
 TEST_CASE("[MultiLogger] Multi threaded") {
-
+	
 	struct MultiLoggerMultiThreadTest;
 
 	using MultiLoggerTest = ska::MultiLogger<
@@ -15,17 +17,23 @@ TEST_CASE("[MultiLogger] Multi threaded") {
 		ska::Logger<ska::LogLevel::Debug, ska::LogLevel::Error, ska::LogSync>
 	>;
 
-	auto ss = std::stringstream{};
-	auto logger = MultiLoggerTest{};
-	logger.get<0>().addOutputTarget(ss);
-	logger.get<1>().addOutputTarget(ss);
+	const auto filename = "toto.txt";
+	{
+		auto ss = std::ofstream{ filename };
 
-	SKA_LOGC_STATIC(logger, ska::LogLevel::Info, MultiLoggerMultiThreadTest) << "test lolilol " << "tututu " << "turlututu chapeau pointu";
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	//SKA_LOGC_STATIC(logger, ska::LogLevel::Info, MultiLoggerMultiThreadTest) << "kékette";
-	
-	logger.terminate();
+		auto logger = MultiLoggerTest{};
+		logger.get<0>().addOutputTarget(ss);
+		logger.get<1>().addOutputTarget(ss);
 
-	std::cout << std::endl << ss.str() << std::endl;
+		SKA_LOGC_STATIC(logger, ska::LogLevel::Info, MultiLoggerMultiThreadTest) << "test lolilol " << "tututu " << "turlututu chapeau pointu";
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
+		logger.terminate();
+	}
+	{
+		auto read = std::ifstream{ filename };
+		std::cout << read.rdbuf();
+	}
+
+	std::remove(filename);
 }
