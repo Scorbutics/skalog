8365f25b0bf541adbaa7a0c321a07f668d6672dc
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-10-30 14:48:05 +0100

    First bench

--- bench/src/main.cpp
+++ bench/src/main.cpp
@@ -5,13 +5,29 @@ using TestLogger = ska::MultiLogger<ska::Logger<>, ska::Logger<>>;
 
 constexpr auto MAX_ITERATIONS = 50000u;
 
-int main() {
-
-    auto logger = TestLogger{};
-
-    for(auto i = 0u; i < MAX_ITERATIONS; i++) {
-        SKA_LOGC_STATIC(logger, ska::LogLevel::Info, TestLogger) << "test";
-    }
-
-    return 0;
+static void BM_StringCreation(benchmark::State& state) {
+      for (auto _ : state)
+              std::string empty_string;
 }
+// Register the function as a benchmark
+ BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+   std::string x = "hello";
+   for (auto _ : state)
+        std::string copy(x);
+}
+ 
+BENCHMARK(BM_StringCopy);
+
+ static void BM_LogInfoTest(benchmark::State& state) {
+     auto logger = TestLogger{};
+
+     for(auto i = 0u; i < MAX_ITERATIONS; i++) {
+          SKA_LOGC_STATIC(logger, ska::LogLevel::Info, TestLogger) << "test";
+       }
+ }
+
+BENCHMARK_MAIN();
+
