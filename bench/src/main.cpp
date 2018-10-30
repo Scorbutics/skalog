f266caa544a49fa9b5e614913a10777639f0bbac
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-10-30 18:23:50 +0100

    Ajout de tests reels

--- bench/src/main.cpp
+++ bench/src/main.cpp
@@ -1,33 +1,53 @@
+#include <iostream>
+#include <sstream>
 #include <benchmark/benchmark.h>
 #include <Logging/MultiLogger.h>
 
-using TestLogger = ska::MultiLogger<ska::Logger<>, ska::Logger<>>;
 
-constexpr auto MAX_ITERATIONS = 50000u;
 
-static void BM_StringCreation(benchmark::State& state) {
-      for (auto _ : state)
-              std::string empty_string;
+static void BM_OutputCoutTest(benchmark::State& state) {
+    std::stringstream ss;  
+    for (auto _ : state) {
+          ss << "test" << std::endl;
+          //ss << "test" << std::endl;
+      }
 }
-// Register the function as a benchmark
- BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-   std::string x = "hello";
-   for (auto _ : state)
-        std::string copy(x);
-}
- 
-BENCHMARK(BM_StringCopy);
 
  static void BM_LogInfoTest(benchmark::State& state) {
-     auto logger = TestLogger{};
-
-     for(auto i = 0u; i < MAX_ITERATIONS; i++) {
+    using TestLogger = 
+        ska::Logger<ska::LogLevel::Info, ska::LogLevel::Error, ska::LogSync>;
+    
+    auto logger = TestLogger{};
+    std::stringstream ss;
+    logger.addOutputTarget(ss);
+    
+    logger.setPattern(ska::LogLevel::Info, "%v");
+    
+    for(auto _ : state) {
           SKA_LOGC_STATIC(logger, ska::LogLevel::Info, TestLogger) << "test";
        }
  }
 
+static void BM_MultiLoggerInfoTest(benchmark::State& state) {
+    using TestLogger = ska::MultiLogger<
+        ska::Logger<ska::LogLevel::Warn, ska::LogLevel::Error, ska::LogSync>, 
+        ska::Logger<ska::LogLevel::Info, ska::LogLevel::Error, ska::LogSync>
+    >;
+    
+    auto logger = TestLogger{};
+    std::stringstream ss;
+    logger.get<0>().addOutputTarget(ss);
+    logger.get<1>().addOutputTarget(ss);
+    
+    logger.get<1>().setPattern(ska::LogLevel::Info, "%v");
+    
+    for(auto _ : state) {
+          SKA_LOGC_STATIC(logger, ska::LogLevel::Info, TestLogger) << "test";
+       }
+ }
+
+BENCHMARK(BM_MultiLoggerInfoTest);
+BENCHMARK(BM_LogInfoTest);
+BENCHMARK(BM_OutputCoutTest);
 BENCHMARK_MAIN();
 
