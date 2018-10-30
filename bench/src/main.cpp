a984eadff88e34958fe1ab53a99cabdd3530ba59
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-10-30 13:27:58 +0100

    Ajout de bench

---
+++ bench/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Logging/MultiLogger.h>
+
+using TestLogger = ska::MultiLogger<ska::Logger<>, ska::Logger<>>;
+
+constexpr auto MAX_ITERATIONS = 50000u;
+
+int main() {
+
+    auto logger = TestLogger{};
+
+    for(auto i = 0u; i < MAX_ITERATIONS; i++) {
+        SKA_LOGC_STATIC(logger, ska::LogLevel::Info, TestLogger) << "test";
+    }
+
+    return 0;
+}
